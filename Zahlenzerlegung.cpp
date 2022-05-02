0afc4dcc679a0225dba8f2f78a47f8e98342effc
Leopold Keller <poldikeller@googlemail.com>
2022-05-02 15:58:06 +0200

    INIT

---
+++ Zahlenzerlegung.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+string Eingabe;
+cout << "Geben Sie eine Zahl ein:" ;
+cin >> Eingabe;
+
+for(auto i : Eingabe){
+cout << i << endl;
+}
+
+
+}
