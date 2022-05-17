0be2f4156519bd2b929c26f216e5647f43dc6ac7
Keller-Kind <poldikeller@googlemail.com>
2022-05-17 18:18:53 +0200

    fixed carray

--- Praktikum3/carray.cpp
+++ Praktikum3/carray.cpp
@@ -6,18 +6,19 @@
 #ifndef CARRAY_CPP
 #define CARRAY_CPP
 
-#include <iostream>
 #include <string.h>
+
+#include <iostream>
 using namespace std;
 
 int main() {
-  int Input, Zwischenwert,usedFields;
+  int Input, Zwischenwert;
+  int usedFields = 0;
   char Eingabe;
   bool running = true;
   int array[20];
   bool arrayFull = false;
-  for (int i = 0; i < 20; i++)
-    array[i] = NULL;
+  for (int i = 0; i < 20; i++) array[i] = 0;
   while (running) {
     cout << "Geben Sie die Aktion ein die sie durchfuehren moechten: " << endl;
     cout << "i - Neuen Wert eingeben" << endl;
@@ -28,61 +29,55 @@ int main() {
     cin >> Eingabe;
 
     switch (Eingabe) {
-    case 'I':
-    case 'i':
-      for (int i = 0; i < 20; i++) {
-        if (array[i] == NULL) {
+      case 'I':
+      case 'i':
+        if (usedFields < 20) {
           cout << "Geben sie den gewuenschten wert ein: ";
           cin >> Input;
-          array[i] = Input;
-          break;
-        usedFields++;
-		}
-        if (i == 19) {
-          cout << "Das Array ist voll!" << endl;
-          arrayFull = true;
+          array[usedFields] = Input;
+          usedFields++;
+
+          if (usedFields == 20) {
+            cout << "Das Array ist voll!" << endl;
+            arrayFull = true;
+          }
         }
-      }
-      break;
-    case 'S':
-    case 's':
-      for (int i = 0; i < 20; i++) {
-        if (array[i] == NULL) {
-          cout << "Das Array hat " << i << " belegte Elemente" << endl;
-          break;
-        }
-      }
-      break;
-    case 'O':
-    case 'o':
-      for (int i = 0; i < 20; i++) {
-        if (array[i] != NULL) {
+        break;
+      case 'S':
+      case 's':
+        cout << "Das Array hat " << usedFields << " belegte Elemente" << endl;
+        break;
+        break;
+      case 'O':
+      case 'o':
+        for (int i = 0; i < usedFields; i++) {
           cout << "Das Element an der Stelle " << i << " ist: " << array[i]
                << endl;
 
-        } else if (array[0] == NULL){
-          cout << "Das Array ist leer!";
-		break;}
-      }
-      break;
-    case 'D':
-    case 'd':
-      if (array[0] == NULL) {
-        cout << "Das Array ist bereits leer!" << endl;
+          if (usedFields == 0) {
+            cout << "Das Array ist leer!";
+            break;
+          }
+        }
         break;
-      }
-      for (int i = 0; i < 19; i++) {
-        if (array[i] != NULL) {
+      case 'D':
+      case 'd':
+        if (usedFields == 0) {
+          cout << "Das Array ist bereits leer!" << endl;
+          break;
+        }
+        for (int i = 0; i < usedFields; i++) {
           array[i] = array[i + 1];
-		}
-        if (i==18) array[19] = NULL;
-     }
-      break;
+        }
+        if (arrayFull) array[19] = 0;
 
-    case 'Q':
-    case 'q':
-      running = false;
-      break;
+        usedFields--;
+        break;
+
+      case 'Q':
+      case 'q':
+        running = false;
+        break;
     }
   }
 }
